797157cf1288cf6dcfc81170b29ef3ae078972c8
Raffael Dela Cruz <160813753+rafdelacruz@users.noreply.github.com>
2025-11-03 22:00:08 -0500

    Add data directories to .gitignore and config.py

--- src/config.py
+++ src/config.py
@@ -3,6 +3,9 @@ from pathlib import Path
 # Directories
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / 'data'
+DATA_RAW_DIR = DATA_DIR / 'raw'
+DATA_SAMPLE_DIR = DATA_DIR / 'sample'
+DATA_PROCESSED_DIR = DATA_DIR / 'processed'
 
 # Audio processing parameters
 TARGET_SR = 16000 # Hz
