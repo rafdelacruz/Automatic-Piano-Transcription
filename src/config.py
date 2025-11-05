6bfa352634e5584ebe97600891cbb19f388a1357
Raffael Dela Cruz <160813753+rafdelacruz@users.noreply.github.com>
2025-11-04 19:34:49 -0500

    Add dataset name and train/val/test directories to config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,19 @@
 from pathlib import Path
 
+DATASET = 'maestro-v3.0.0'
+SPLIT_CONFIGURATION_FILE = 'maestro-v3.0.0.csv'
+
 # Directories
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATA_DIR = BASE_DIR / 'data'
 DATA_RAW_DIR = DATA_DIR / 'raw'
 DATA_SAMPLE_DIR = DATA_DIR / 'sample'
 DATA_PROCESSED_DIR = DATA_DIR / 'processed'
+
+TRAIN_DIR = DATA_PROCESSED_DIR / 'train'
+VAL_DIR = DATA_PROCESSED_DIR / 'val'
+TEST_DIR = DATA_PROCESSED_DIR / 'test'
 
 # Audio processing parameters
 TARGET_SR = 16000 # Hz
