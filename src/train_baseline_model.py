e968b9dca38eccf211a479717c68af40a31aff51
Raffael Dela Cruz <160813753+rafdelacruz@users.noreply.github.com>
2025-11-06 08:54:40 -0500

    Add code to save training and validation lossses across epochs

--- src/train_baseline_model.py
+++ src/train_baseline_model.py
@@ -2,6 +2,9 @@
 # Based on: Kelz et al., "On the Potential of Simple Framewise Approaches to Piano Transcription"
 # https://arxiv.org/pdf/1612.05153
 
+import pathlib
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,8 +12,10 @@ from torch.utils.data import DataLoader
 
 from models.baseline import AllConv
 
-def train(model, train_data, val_data=None, batch_size=16, num_epochs=10, save_path=None):
+def train(model, train_data, val_data=None, batch_size=16, num_epochs=10, save_path=None, save_checkpoint=True):
     torch.manual_seed(42)
+    train_losses = np.zeros(num_epochs)
+    val_losses = np.zeros(num_epochs)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
@@ -59,14 +64,20 @@ def train(model, train_data, val_data=None, batch_size=16, num_epochs=10, save_p
 
         avg_train_loss = train_loss / len(train_loader)
         avg_val_loss = val_loss / len(val_loader)
+        train_losses[epoch] = avg_train_loss
+        val_losses[epoch] = avg_val_loss
+
         scheduler.step()
 
-        # Save checkpoint
-        if save_path:
-            torch.save(model.state_dict(), 'state.pth')
+        # Save checkpoints
+        if save_checkpoint and save_path:
+            torch.save(model.state_dict(), 'state.pth') # TODO: Create function to obtain model name (use for checkpoint file name)
 
         print(f'Epoch {epoch + 1}/{num_epochs} | Train Loss: {avg_train_loss:.4f} | Validation Loss: {avg_val_loss:.4f}')
 
+    if save_path:
+        np.savetxt(save_path / 'train_loss.csv', train_losses)
+        np.savetxt(save_path / 'val_loss.csv', val_losses)
 
 if __name__ == '__main__':
     model = AllConv()
