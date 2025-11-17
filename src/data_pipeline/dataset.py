a868817a78245914d6f7512b5ecde56809643bb7
Raffael Dela Cruz <160813753+rafdelacruz@users.noreply.github.com>
2025-11-17 11:15:08 -0500

    Add documentation to dataset.py

--- src/data_pipeline/dataset.py
+++ src/data_pipeline/dataset.py
@@ -1,9 +1,45 @@
 import numpy as np
+import pathlib
 import torch
 from torch.utils.data import Dataset
 
 class PianoTranscriptionDataset(Dataset):
-    def __init__(self, data_dir, split, augment=False, max_samples=None):
+    """
+    PyTorch Dataset for loading precomputed log-mel spectrogram segments and
+    corresponding piano-roll labels for piano transcription.
+
+    Parameters
+    ----------
+    data_dir : pathlib.Path
+        A Path object pointing to the data directory to be loaded.
+    split : {'train', 'val', 'test'}
+        A string representing which dataset split to load
+    augment : bool, default=False
+        Indicates whether or to apply data augentations.
+    max_samples : int, default=None
+        If provided, limits the number of samples loaded.
+
+    Attributes
+    ----------
+    log_mel_segments : torch.Tensor
+        Tensor of shape (n_segments, 1, n_frames, n_mels) containing the
+        log_mel spectrograms.
+    piano_roll_segments : torch.Tensor
+        Tensor of shape (n_segments, n_frames, 88) for the primary model data
+        or shape (n_segments, 88) for the baseline model data, containing the
+        piano_roll labels.
+    pos_weight : torch.Tensor
+        Per-note positive class weight for handling imbalance (only for
+        training set).
+
+    """
+    def __init__(
+        self,
+        data_dir: pathlib.Path,
+        split: str,
+        augment: bool = False,
+        max_samples: int = None
+    ) -> None:
         log_mel_file_name = split + '_log_mel_segments.npy'
         piano_roll_file_name = split + '_piano_roll_segments.npy'
 
@@ -38,10 +74,35 @@ class PianoTranscriptionDataset(Dataset):
         self.log_mel_segments = torch.tensor(self.log_mel_segments, dtype=torch.float32).unsqueeze(1)
         self.piano_roll_segments = torch.tensor(self.piano_roll_segments, dtype=torch.float32)
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Return the number of samples in the dataset.
+
+        Returns
+        -------
+        int
+            Total number of (log-mel, piano-roll) pairs.
+        """
         return len(self.log_mel_segments)
     
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Retrieve a single sample pair.
+
+        Parameters
+        ----------
+        idx : int
+            The index of the sample to retrieve.
+
+        Returns
+        -------
+        tuple[torch.Tensor, torch.Tensor]
+            A tuple containing:
+            - torch.Tensor
+                Tensor of log-mel spectrograms.
+            - torch.Tensor
+                Tensor of piano-roll labels.
+        """
         x = self.log_mel_segments[idx]
         y = self.piano_roll_segments[idx]
 
