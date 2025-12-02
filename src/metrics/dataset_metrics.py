476572a65e56ac2a8e840b7a7b359db0669f8905
Raffael Dela Cruz <160813753+rafdelacruz@users.noreply.github.com>
2025-12-01 19:32:43 -0500

    Create function to count number of note activations across a dataset in dataset_metrics.py

--- src/metrics/dataset_metrics.py
+++ src/metrics/dataset_metrics.py
@@ -1,5 +1,17 @@
 import numpy as np
 
+from data_pipeline.dataset import PianoTranscriptionDataset
+
 # Assume input: [n_segments, 88, n_frames]
 def count_note_activations(piano_roll: np.ndarray) -> np.ndarray:
     return piano_roll.sum(axis=(0, 2)).astype(np.int32)
+
+# Assume piano roll tensors: [n_segments, n_frames, 88]
+def count_note_activations_in_dataset(dataset: PianoTranscriptionDataset) -> np.ndarray:
+    activations = np.zeros(88, dtype=np.int32)
+
+    for _, piano_roll in dataset:
+        piano_roll = piano_roll.numpy()
+        activations += piano_roll.sum(axis=(0)).astype(np.int32)
+
+    return activations
